aa8e7f3002b91d55e2f0de2c29a47b91b72a748e
MeoChuot <chidungdekiemtra@gmail.com>
2019-01-26 10:11:24 +0700

    update audio

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main">
+        <div class="content">
+            <div class="player">
+                <vue-plyr>
+                    <div data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
+                </vue-plyr>
+                
+            </div>
+        </div>
+        
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+        return {
+            source: "/static/Em-Se-La-Co-Dau-Minh-Vuong-M4U-Huy-Cung.mp3"
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VuePlyr from 'vue-plyr'
+import 'vue-plyr/dist/vue-plyr.css'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.use(VuePlyr)
 
+Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
